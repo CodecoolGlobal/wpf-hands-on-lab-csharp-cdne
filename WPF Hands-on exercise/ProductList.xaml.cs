cd416bd4fc469dc7b2e329f93d41ea26315cef30
Claudiu <cduneata@gmail.com>
2020-06-26 21:12:44 +0300

    Add solution from pdf file

--- WPF Hands-on exercise/ProductList.xaml.cs
+++ WPF Hands-on exercise/ProductList.xaml.cs
@@ -1,15 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using HandsOnLab1.ClientEntities;
 using HandsOnLab1.ServiceAgents;
 
@@ -20,12 +8,21 @@ namespace HandsOnLab1
     /// </summary>
     public partial class ProductList : System.Windows.Controls.UserControl
     {
+        private ProductSummaryCollection _products;
+
+        public ProductSummaryCollection Products
+        {
+            get { return _products; }
+        }
+
         /// <summary>
         /// Initializes a new instance of the <see cref="ProductList"/> class.
         /// </summary>
         public ProductList()
         {
             InitializeComponent();
+            _products = ProductAgent.GetProductList();
+            DataContext = this;
         }
     }
 }
